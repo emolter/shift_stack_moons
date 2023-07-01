02b1160e84459ab91da721f766ce2f2f54ce2d97
Edward Molter <emolter@berkeley.edu>
2023-06-30 20:32:40 -0700

    version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = '0.0.7'
+VERSION = '0.1.0'
 DESCRIPTION = "Find small moons around planets \
     using shift-and-stack based on JPL Horizons ephemeris"
 
