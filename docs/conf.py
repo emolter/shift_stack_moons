333c42e5b14dfc7559cd2d6565f6e0ea0c2d9c03
Edward Molter <emolter@berkeley.edu>
2023-07-06 18:36:19 -0700

    fixing readthedocs build

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -18,7 +22,6 @@ extensions = ['sphinx.ext.autodoc', 'nbsphinx']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 
 # -- Options for HTML output -------------------------------------------------
